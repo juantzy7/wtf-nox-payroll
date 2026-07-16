14d39d830dc99f2c2ccb7b45677f5b6547f5232a
x <x@x.co>
2026-07-17 05:48:15 +0800

    Remove stray app_config.js

--- app_config.js
+++
@@ -1,3 +0,0 @@
-// Frontend config. Edit WC_PROJECT_ID with your WalletConnect Cloud project id.
-// Get one free at https://cloud.walletconnect.com (create project -> copy id).
-export const WC_PROJECT_ID = "YOUR_WALLETCONNECT_PROJECT_ID";
