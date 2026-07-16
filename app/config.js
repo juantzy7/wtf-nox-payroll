edb1281a828e6d3627320079c27544f686f6a217
x <x@x.co>
2026-07-17 05:55:42 +0800

    Set WalletConnect project id

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 // Frontend config. Edit WC_PROJECT_ID with your WalletConnect Cloud project id.
 // Get one free at https://cloud.walletconnect.com (create project -> copy id).
-export const WC_PROJECT_ID = "YOUR_WALLETCONNECT_PROJECT_ID";
+export const WC_PROJECT_ID = "5ac5e41049928c7f99a0706b06c7b7b5";
